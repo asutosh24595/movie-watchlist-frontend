0213912d2586de2dd7a1c86b0f247e8c473f0d90
Asutosh <asutoshshukla95@gmail.com>
2024-07-03 18:06:32 +0530

    initial commit

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 axios.defaults.withCredentials = true;
-const API_BASE_URL = "https://movie-watchlist-backend-aoz5.vercel.app";
+const API_BASE_URL = "https://movie-watchlist-backend-neon.vercel.app";
 
 export const addMovieToDb = async (movieData) => {
   try {
